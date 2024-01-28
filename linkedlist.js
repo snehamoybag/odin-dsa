bff15996878204b03f13563ad84dcb61887e3134
snehamoy bag <snehamoybag2@gmail.com>
2024-01-28 11:18:27 +0530

    implement 'at(index)' functionality

--- linkedlist.js
+++ linkedlist.js
@@ -16,6 +16,21 @@ class LinkedList {
     this.size -= 1;
   }
 
+  #generateIndiciesHashMap() {
+    const map = {};
+    let count = 0;
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      map[count] = currentNode;
+
+      currentNode = currentNode.next;
+      count++;
+    }
+
+    return map;
+  }
+
   prepend(value) {
     const node = new Node(value);
 
@@ -37,11 +52,25 @@ class LinkedList {
       this.head = node;
       this.tail = node;
     } else {
-      this.tail.next = node;
-      this.tail = node;
+      this.tail.next = node; // append node
+      this.tail = node; // update tail to be the latest last node
     }
 
     this.#incrementSize();
+  }
+
+  at(index) {
+    if (
+      isNaN(index) ||
+      !Number.isSafeInteger(index) ||
+      index < 0 ||
+      index >= this.size
+    ) {
+      return null;
+    }
+
+    const indiciesHashMap = this.#generateIndiciesHashMap();
+    return indiciesHashMap[index];
   }
 }
 
@@ -54,6 +83,4 @@ linkedList.append("hello4");
 linkedList.prepend("hello3");
 
 console.log(linkedList);
-console.log(linkedList.head);
-console.log(linkedList.tail);
-console.log(linkedList.size);
+console.log(linkedList.at(-1));
